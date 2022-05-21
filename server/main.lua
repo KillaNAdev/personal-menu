c62ba25172d2c842df7221931bb70479139b71f9
KillaNA <90366824+KillaNAdev@users.noreply.github.com>
2022-05-21 17:43:01 -0500

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -1,6 +1,3 @@
-ESX = nil
-
-TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
 CreateThread(function()
 	local CurrentVersion = GetResourceMetadata(GetCurrentResourceName(), 'version', 0)
